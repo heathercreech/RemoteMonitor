eeda2f988a3e04fb9877b6a2553214d08ba25054
Seth Creech <sacreech@moreheadstate.edu>
2016-02-01 22:06:01 -0500

    Added protocol specifier to requests.get() call

--- rmon_requests.py
+++ rmon_requests.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def sendClientRequest(ip, call="update"):
-	encoded_json_data = requests.get(ip + "/" + call).content
+	encoded_json_data = requests.get("http://" + ip + "/" + call).content
 	return json.loads(str(encoded_json_data.decode("utf-8")));
