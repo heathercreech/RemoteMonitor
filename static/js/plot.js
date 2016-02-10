950ec3d7ea51978553dda9fe7111c2c145eec182
Seth Creech <sacreech@moreheadstate.edu>
2016-02-09 22:54:02 -0500

    Added automatic chart generation system

--- static/js/plot.js
+++ static/js/plot.js
@@ -2,7 +2,8 @@
 //	Charts.js
 
 
-var charts = []
+var charts = {}
+var max_points = 10;
 
 
 //creates a chart's data structure from an array of labels and raw data
@@ -25,23 +26,71 @@ function formatData(labels, chart_data){
 }
 
 
-function getChartContext(chart_id){
-	return $("#" + chart_id).get(0).getContext("2d");
+//generates all the elements required to display a chart -- value_source is where the data came from (i.e. 'cpu') and value_type is what the value represents (i.e. 'usage')
+function generateChart(value_source, value_type){
+	var chart_container = document.createElement("div");
+	chart_container.className = "app-item";
+	
+	chart_container.appendChild(generateChartHeader(value_source, value_type));
+	chart_container.appendChild(generateChartCanvas(value_source, value_type));
+	
+	return chart_container;
 }
 
 
-function addLineChart(chart_id, data){
-	charts.push({
-		key: chart_id,
-		value: new Chart(getChartContext(chart_id)).Line(data, {
+//generates a charts header -- value_source is where the data came from (i.e. 'cpu') and value_type is what the value represents (i.e. 'usage')
+function generateChartHeader(value_source, value_type){
+	var chart_header = document.createElement("h3");
+	chart_header.innerHTML = value_source + " " + value_type;
+	return chart_header;
+}
+
+
+//generates a charts canvas -- value_source is where the data came from (i.e. 'cpu') and value_type is what the value represents (i.e. 'usage')
+function generateChartCanvas(value_source, value_type){
+	var chart_canvas = document.createElement("canvas");
+	var sq_gph = 200;
+	
+	chart_canvas.width = sq_gph;
+	chart_canvas.height = sq_gph;
+	
+	
+	//generate the id of the chart
+	var chart_id = value_source + "_" + value_type;
+	
+	chart_canvas.setAttribute("id", chart_id);
+	console.log(chart_canvas);
+	//chart_canvas.getContext("2d");
+	addLineChart(chart_canvas, formatData([], []));
+	
+	return chart_canvas;
+}
+
+
+//returns a chart's canvas context
+function getChartContext(chart_canvas){
+	return chart_canvas.getContext("2d");
+}
+
+
+//sets a line chart's visual options and adds it to the charts array
+function addLineChart(chart_canvas, data){
+	charts[chart_canvas.getAttribute("id")] = new Chart(getChartContext(chart_canvas)).Line(data, {
 			scaleLineColor: "rgba(200,200,200,1)",
 			scaleFontColor: "rgba(200,200,200,1)",
-			scaleOverride: true,
-			scaleSteps: 10,
-			scaleStepWidth: 10,
-			scaleStartValue: 0,
-			scaleBeginsAtZero: true,
-			animationSteps: 15
-		})
-	});
+		});
+}
+
+
+//adds a data point to a chart and removes one if there are too many points
+function addChartPoint(chart_id, point_value){
+	var target_graph = charts[chart_id];
+	
+	if(target_graph != undefined){
+		console.log(target_graph);
+		if(target_graph.datasets[0].points.length > max_points){
+			target_graph.removeData();
+		}
+		target_graph.addData([point_value], "");
+	}
 }
